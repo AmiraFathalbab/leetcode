303c82139f4da093f1b5908dfd4ab3a4ee753ad7
AmiraFathalbab <amira.fathalbab@gmail.com>
2025-06-01 01:08:10 +0300

    Time: 224 ms (24.57%), Space: 29.5 MB (16.3%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cs
@@ -0,0 +1,50 @@
+public class Solution {
+    public int Divide(int dividend, int divisor) {
+        int res=0;
+        long dividend1=dividend;
+        long divisor1=divisor;
+        if(dividend1==-2147483648&&divisor1==-1)
+        {return 2147483647;}
+        if(divisor1==1)
+        {return dividend;}
+        if(Math.Abs(dividend1)<Math.Abs(divisor1))
+        {return res;}
+        if((dividend1<0&&divisor1<0) ||(dividend1>0&&divisor1>0))
+        {
+            for(int n=20;n>0;n--){
+            while(Math.Abs(dividend1)>=n*Math.Abs(divisor1))
+            {
+                dividend1=Math.Abs(dividend1)-n*Math.Abs(divisor1);
+                
+                res+=n;
+            }
+            }
+            return res;
+
+        }
+        else {//if((dividend<0&&divisor>0) ||(dividend>0&&divisor<0)){
+            //try{
+            for(int n=60;n>0;n--){
+            while(Math.Abs(dividend1)>=n*Math.Abs(divisor1))
+            {
+                dividend1=Math.Abs(dividend1)-n*Math.Abs(divisor1);
+                
+                res-=n;
+            }
+            }
+            
+            // catch(Exception e)
+            // {
+            //     return -2147483648;
+            // }
+            
+            
+            return res;
+        }
+        // else
+        //     return res;
+
+        
+        
+    }
+}
