99ca2d5e1902bd8d0e35d2e0cc7debb3ba1fe6f1
AmiraFathalbab <amira.fathalbab@gmail.com>
2025-06-01 21:19:01 +0300

    Time: 77 ms (5.02%), Space: 55.4 MB (5.24%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cs
@@ -0,0 +1,76 @@
+public class Solution {
+    public int RomanToInt(string s) {
+        int res= 0;
+        //string[] r=["I","V","X","L","C","D","M"];
+        for(int i=0;i<s.Length;i++)
+        {
+            if(string.Equals(s[i], 'I')&&i!=s.Length-1&&string.Equals(s[i+1], 'V'))
+            {
+                res+=4;
+                i+=1;
+            }
+            else if(string.Equals(s[i], 'I')&&i!=s.Length-1&&string.Equals(s[i+1], 'X'))
+            {
+                res+=9;
+                i+=1;
+            }
+            else if(string.Equals(s[i], 'I'))
+            {
+                res+=1;
+            }
+            else if(string.Equals(s[i], 'V'))
+            {
+                res+=5;
+            }
+
+            
+            else if(string.Equals(s[i], 'X')&&i!=s.Length-1&&string.Equals(s[i+1], 'L'))
+            {
+                res+=40;
+                i+=1;
+            }
+            else if(string.Equals(s[i], 'X')&&i!=s.Length-1&&string.Equals(s[i+1], 'C'))
+            {
+                res+=90;
+                i+=1;
+            }
+
+            else if(string.Equals(s[i], 'X'))
+            {
+                res+=10;
+            }
+            else if(string.Equals(s[i], 'L'))
+            {
+                res+=50;
+            }
+            
+           else if(string.Equals(s[i], 'C')&&i!=s.Length-1&&string.Equals(s[i+1], 'D'))
+            {
+                res+=400;
+                i+=1;
+            }
+            else if(string.Equals(s[i], 'C')&&i!=s.Length-1&&string.Equals(s[i+1], 'M'))
+            {
+                res+=900;
+                i+=1;
+            }
+            else if(string.Equals(s[i], 'C'))
+            {
+                res+=100;
+            }
+            else if(string.Equals(s[i], 'D'))
+            {
+                res+=500;
+            }
+            else if(string.Equals(s[i], 'M'))
+            {
+                res+=1000;
+            }
+
+            
+Console.WriteLine(i);
+        }
+        
+        return res;
+    }
+}
